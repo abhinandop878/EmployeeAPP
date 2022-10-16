6c2999abd38ea44bef1196ade33d0eb8ee781770
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-17 01:51:43 +0530

    Employee App

---
+++ src/test/java/com/nestDigital/EmployeeAPP/EmployeeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestDigital.EmployeeAPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
